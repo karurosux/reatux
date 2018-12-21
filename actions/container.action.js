02994e76579396812fe91ec19bf269f9736d67f7
Carlos Gonzalez <carlosgonzalez@pop-os.localdomain>
2018-12-20 22:23:46 -0800

    "force" message changed to "update"

--- actions/container.action.js
+++ actions/container.action.js
@@ -1,16 +1,17 @@
 const { projectFileHelper } = require('../helpers');
 
-const createContainerComponent = (props) => (name) => {
-	return () => {
-		const { env } = props;
-		env.run('container', {
-			name
-		});
-	};
+const createContainerComponent = props => name => {
+  return () => {
+    const { env } = props;
+    env.run('container', {
+      name,
+      force: true
+    });
+  };
 };
 
-module.exports = (props) => (...args) => {
-	projectFileHelper
-		.ifProjectFolder()
-		.then(createContainerComponent(props)(...args));
+module.exports = props => (...args) => {
+  projectFileHelper
+    .ifProjectFolder()
+    .then(createContainerComponent(props)(...args));
 };
